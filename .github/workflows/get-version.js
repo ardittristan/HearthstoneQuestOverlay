327af214e948865d6649ca8399ba246c9286d01e
ardittristan <42787508+ardittristan@users.noreply.github.com>
2022-03-13 05:20:00 +0100

    Fix path

--- .github/workflows/get-version.js
+++ .github/workflows/get-version.js
@@ -1,2 +1,2 @@
 var fs = require("fs");
-console.log(fs.readFileSync("module.json", "utf8").match(/(?<=^[^\/]+AssemblyVersion\(")([0-9]+\.[0-9]+\.[0-9]+)/m)?.[0]);
+console.log(fs.readFileSync("QuestOverlayPlugin/Properties/AssemblyInfo.cs", "utf8").match(/(?<=^[^\/]+AssemblyVersion\(")([0-9]+\.[0-9]+\.[0-9]+)/m)?.[0]);
